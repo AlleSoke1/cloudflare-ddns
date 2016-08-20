5ce5872af14d26c9f1726362048c3305fcece8f0
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-08-20 09:47:39 +0800

    Only update records if connected to certain SSIDs

--- cloudflare-ddns.py
+++ cloudflare-ddns.py
@@ -3,6 +3,7 @@
 ##it updates cloudflare records on a specified zone and subdomain with your current ip (i use it for a home server)
 import urllib2
 import json
+import commands
 
 #settings
 EMAIL="cf-email@mail.com"
@@ -15,8 +16,22 @@ SUBDOMAINS=[
 	"cloud.hackalin.me"
 ]
 
-myip = urllib2.urlopen("https://wtfismyip.com/text").read().rstrip('\n')
+ONLYSSIDS = [
+  "adita"
+]
 
+ssid = commands.getstatusoutput("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'")[1]
+
+found = True
+try:
+  ONLYSSIDS.index( ssid )
+except ValueError, e:
+  found = False
+
+if False == found:
+  exit(0)
+
+myip = urllib2.urlopen("https://wtfismyip.com/text").read().rstrip('\n')
 
 def getJSONrecords():
   API_REQUEST = "https://www.cloudflare.com/api_json.html?a=rec_load_all&tkn="+API_KEY+"&email="+EMAIL+"&z="+ZONE
@@ -27,8 +42,6 @@ def update_record(recordid,recordname,servicetype):
   recordname = recordname.partition(".")[0]
   API_REQUEST = "https://www.cloudflare.com/api_json.html?a=rec_edit&tkn="+API_KEY+"&id="+recordid+"&email="+EMAIL+"&z="+ZONE+"&type=A&name="+recordname+"&content="+myip+"&service_mode="+servicetype+"&ttl=1"
   urllib2.urlopen(API_REQUEST)
-  #print API_REQUEST
-
 
 jdata = getJSONrecords()
 
